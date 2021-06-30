f17dc45b380c8e636ade9afc5ae8cd697ce1594c
Raphaella Robles <raphaella.robles@gmail.com>
2021-06-30 20:02:53 +0000

    Set up connection with snake game server

---
+++ play.js
@@ -0,0 +1,22 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  // Create conn object that represents server connection
+  const conn = net.createConnection({
+    host: '135.23.223.133',
+    port: 50542
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // handle incoming data and print it for player
+  conn.on("data", (data) => {
+    console.log(`Server says: ${data}`);
+  });
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
